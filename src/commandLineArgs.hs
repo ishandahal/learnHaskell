b06ba834852232189007e4f7a25f7f1ac2010e65
ishandahal <ishan.dahal88@gmail.com>
2025-01-27 15:39:27 -0500

    Handle commandline arguments

---
+++ src/commandLineArgs.hs
@@ -0,0 +1,36 @@
+import Data.List (delete)
+import System.Directory (removeFile, renameFile)
+import System.Directory.Internal.Prelude (getArgs, hClose, hPutStr)
+import System.IO (openFile, openTempFile)
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch =
+  [ ("add", add),
+    ("display", display),
+    ("remove", remove)
+  ]
+
+main = do
+  (command : args) <- getArgs
+  let Just action = lookup command dispatch
+  action args
+
+add :: [String] -> IO ()
+add [fileName, task] = appendFile fileName (task ++ ['\n'])
+
+display :: [String] -> IO ()
+display [fileName] = do
+  contents <- readFile fileName
+  mapM_ putStrLn (lines contents)
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+  (tempFile, tempHandle) <- openTempFile "." "temp"
+  contents <- readFile fileName
+  let allTasks = lines contents
+      number = read numberString
+      updatedTasks = delete (allTasks !! number) allTasks
+  hPutStr tempHandle (unlines updatedTasks)
+  hClose tempHandle
+  removeFile fileName
+  renameFile tempFile fileName
